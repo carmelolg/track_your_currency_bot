08b18af7518b14542baa12831e3339067f0ca9c2
carmelolg <carmelolagamba@hotmail.it>
2021-01-07 00:06:46 +0100

    test mapper

--- currency/mapper.js
+++ currency/mapper.js
@@ -15,6 +15,8 @@ var map = function (response) {
     data.symbol = data.symbol;
     data.rank = data.cmc_rank;
 
+    console.log(data);
+
     quote = data.quote['EUR'];
     data.priceEUR = CurrencyUtils.convertToCurrency(quote.price, 2);
     data.marketCapEUR = CurrencyUtils.convertToCurrency(quote.market_cap);
